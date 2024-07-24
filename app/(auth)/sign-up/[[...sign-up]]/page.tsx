3b973850a938281c7b3df38d9cdc51b62c4eba10
umer <you@example.com>
2024-07-24 15:28:24 +0500

    initial commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const SignUpPag = () => {
+  return <SignUp />
+}
+
+export default SignUpPag
---
+++ lib/database/models/transaction.model.ts
@@ -0,0 +1,14 @@
+import { Document, model, models, Schema } from "mongoose";
+
+const TransactionSchema = new Schema({
+    createdAt: { type: Date, required: Date.now },
+    stripeId: { type: String, required: true, unique: true },
+    amount: { type: Number, required: true },
+    plan: { type: String },
+    credits: { type: Number },
+    buyer: { type: Schema.Types.ObjectId },
+})
+
+const Transaction = models?.Transaction || model('Transaction', TransactionSchema);
+
+export default Transaction;
