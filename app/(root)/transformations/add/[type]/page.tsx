cf8a1cc602b090014adca72ac689b849e7dd565f
umer <you@example.com>
2024-07-30 13:12:05 +0500

    implementing clerk webhooks

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -26,9 +26,9 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
       <section className="mt-10">
         <TransformationForm 
           action="Add"
-          userId={user?.id}
+          userId={user._id}
           type={transformation.type as TransformationTypeKey}
-          creditBalance={user?.creditBalance}
+          creditBalance={user.creditBalance}
         />
       </section>
     </>
